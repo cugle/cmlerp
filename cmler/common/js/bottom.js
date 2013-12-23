5f2439b4d3b1a908f1e1b542c0b5aed5f6ff6cf7
cugle <452275147@qq.com>
2013-12-23 23:02:17 +0800

    1.0

--- cmler/common/js/bottom.js
+++
@@ -1,5 +0,0 @@
-if(state==0){
-alert('发送失败')}else
-{alert('发送成功')
-	}
-	location.href='messagerecord.php';
--- cmler/customer/test-.php
+++
@@ -1,4 +0,0 @@
-<?php
-date_default_timezone_set('PRC');
-echo date("Y-m-d H:i:s",strtotime('2010-08-12 12:00:00'))
-?>
